8ac2719934dd19b2ffdeb411739edc02fa0f45a4
Glitch (miniature-hamster) <none>
2019-08-16 14:51:05 +0000

    🍢🕍 Checkpoint ./server.js:6074723/923

--- server.js
+++ server.js
@@ -23,23 +23,27 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
      
-// res.send(new Date(parseInt(req.params.date_string)))
-app.get("/api/timestamp/:date_string",(req,res,next)=>{
-    if(new Date(req.params.date_string)){
-      // If the date_string makes a valid date
-      next();
-    }else if(new Date(parseInt(req.params.date_string))){
-      let date = new Date(parsereq.params.date_string);
+/*else if(new Date(parseInt(req.params.date_string))){
+      let date = new Date(parseInt(req.params.date_string));
       res.json({"unix":date.getTime(),"utc":date.toUTCString()})
-             }
-    else{
-      res.json({"error":"Invalid Date"})
-    }
-  },
+    }*/
+
+app.get("/api/timestamp/:date_string",
+    // (req,res,next)=>{
+    //   if(new Date(req.params.date_string)){
+    //     // If the date_string makes a valid date
+    //     next();
+    //   }
+    //   else{
+    //     res.json({"error":"Invalid Date"})
+    //   }
+    // },
    (req,res)=>{
-    let date = new Date(req.params.date_string);
-    res.json({"unix":date.getTime(),"utc":date.toUTCString()})
-  res.send(new Date(req.params.date_string));
+  let date = new Date(parseInt(req.params.date_string));
+  res.send(date.to)
+  //   let date = new Date(req.params.date_string);
+  //   res.json({"unix":date.getTime(),"utc":date.toUTCString()})
+  // res.send(new Date(req.params.date_string));
 })
 
 
