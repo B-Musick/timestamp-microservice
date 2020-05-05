df9a230ba88b798552611d7752c75affa3084e8c
Glitch (miniature-hamster) <none>
2020-05-05 14:31:54 -0500

    Done programming, styled and logic complete

--- server.js
+++ server.js
@@ -16,49 +16,35 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.get("/", (req, res) => { res.sendFile(__dirname + '/views/index.html'); });
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-     
-/*else if(new Date(parseInt(req.params.date_string))){
-      let date = new Date(parseInt(req.params.date_string));
-      res.json({"unix":date.getTime(),"utc":date.toUTCString()})
-    }*/
+app.get("/api/hello", (req, res) => { res.json({greeting: 'hello API'}); });
 
+// Timestamp with no date given, current date is returned
 app.get("/api/timestamp",(req,res)=>{
   let date = new Date();
   res.json({"unix":date.getTime(),"utc":date.toUTCString()});
-
 });
 
 app.get("/api/timestamp/:date_string",
     (req,res)=>{
-    
       let date;
       if((new Date(req.params.date_string)).getTime()){
         // If the date_string makes a valid date
         date = new Date(req.params.date_string);
         res.json({"unix":date.getTime(),"utc":date.toUTCString()});
       }else if((new Date(parseInt(req.params.date_string)))){
+        // If it is an integer passed
          date = new Date(parseInt(req.params.date_string));
          res.json({"unix":date.getTime(),"utc":date.toUTCString()});
       }
-      else{
-        res.json({"error":"Invalid Date"})
-      }
-    
+      // If none of the above work then error
+      else res.json({"error":"Invalid Date"});
     }
 )
 
-
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+// listen for requests 
+var listener = app.listen(process.env.PORT, ()=> {
   console.log('Your app is listening on port ' + listener.address().port);
 });
